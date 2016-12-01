6e9821fc231e64984dcc50b9b84ca5965acddab4
HadrienMP <public@hadrienmp.fr>
2016-12-01 20:26:28 +0100

    Hiegel Calculator : Multiply

--- hiegel_calculator/src/main/java/perso/_1/Calculator.java
+++ hiegel_calculator/src/main/java/perso/_1/Calculator.java
@@ -1,31 +1,37 @@
 package perso._1;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
 
 class Calculator {
 
+    public static final String NUMBER_REGEX = "-?\\d+";
+    public static final String OPERATIONS_REGEX = "[*+-]";
+
     int compute(String operations) {
-        if (operations == null) {
+
+        if (areLegal(operations)) {
             throw new IllegalArgumentException();
         }
 
         String addOperations = replaceSubstractionsByAdditions(operations);
-        List<Integer> toAdd = getNumbersToAdd(addOperations);
 
-        if (toAdd.isEmpty()) {
-            throw new IllegalArgumentException();
+        Stream<Integer> integerStream = Arrays.stream(addOperations.split("[*+]"))
+                .filter(this::isANumber)
+                .map(Integer::parseInt);
+
+        if (operations.contains("*")) {
+            return integerStream.reduce(1, this::multiply);
         }
 
-        return toAdd.stream().reduce(0, this::add);
+        return integerStream.reduce(0, this::add);
     }
 
-    private List<Integer> getNumbersToAdd(String addOperations) {
-        return Arrays.stream(addOperations.split("\\+"))
-                .filter(this::isANumber)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+    private boolean areLegal(String operations) {
+        return operations == null || !operations.matches("(" + NUMBER_REGEX + OPERATIONS_REGEX + ")*" + NUMBER_REGEX);
     }
 
     private String replaceSubstractionsByAdditions(String operations) {
@@ -33,7 +39,29 @@ class Calculator {
     }
 
     private boolean isANumber(String s) {
-        return s.matches("-?\\d+");
+        return s.matches(NUMBER_REGEX);
+    }
+
+    private int multiply(Integer integer, Integer integer2) {
+        // TODO tester multiplication par 0 qui ne doit pas planter ici
+        if (areMultipliable(integer, integer2)) {
+            return integer * integer2;
+        }
+
+        throw new IntegerOverflowException();
+    }
+
+    private boolean areMultipliable(Integer integer, Integer integer2) {
+        // TODO tester avec les valeurs extremes dans les multiplications
+        if (areOfSameSign(integer, integer2)) {
+            return MAX_VALUE / Math.abs(integer) >= Math.abs(integer2);
+        } else {
+            return MIN_VALUE / Math.abs(integer) <= -Math.abs(integer2) || (integer == MIN_VALUE && integer2 == 1);
+        }
+    }
+
+    private boolean areOfSameSign(Integer integer, Integer integer2) {
+        return integer < 0 && integer2 < 0 || integer > 0 && integer2 > 0;
     }
 
     private int add(Integer first, Integer second) {
@@ -44,12 +72,11 @@ class Calculator {
         throw new IntegerOverflowException();
     }
 
-
     private boolean areAddable(Integer first, Integer second) {
         if (first > 0 && second > 0) {
-            return Integer.MAX_VALUE - first > second;
+            return MAX_VALUE - first > second;
         } else if (first < 0 && second < 0) {
-            return Integer.MIN_VALUE - first < second;
+            return MIN_VALUE - first < second;
         } else {
             return true;
         }
--- hiegel_calculator/src/test/java/perso/_1/CalculatorTest.java
+++ hiegel_calculator/src/test/java/perso/_1/CalculatorTest.java
@@ -5,14 +5,43 @@ import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.Objects;
-
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(JUnitParamsRunner.class)
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
+
+    //--------------------------------------------------------------------------
+    // BASICS
+    //--------------------------------------------------------------------------
+
+    @Test
+    public void should_return_the_number_when_it_is_alone_in_the_operations() {
+        String operations = "1";
+        int result = calculator.compute(operations);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_an_exception_when_the_number_is_bigger_than_the_integer_max_values() {
+        String operations = MAX_VALUE + "0";
+        should_throw_an_exception_when_trying_to_add_something_else_than_numbers(operations);
+    }
+
+    @Parameters({"", "    ", "blabla"})
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_an_exception_for_operations_not_respecting_the_add_pattern(String operations) {
+        should_throw_an_exception_when_trying_to_add_something_else_than_numbers(operations);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_an_exception_for_a_null_string() {
+        should_throw_an_exception_when_trying_to_add_something_else_than_numbers(null);
+    }
+
 
     //--------------------------------------------------------------------------
     // ADD
@@ -27,10 +56,10 @@ public class CalculatorTest {
             "1+-2, -1",
             "-2+1, -1",
 
-            Integer.MAX_VALUE + "+0, " + Integer.MAX_VALUE,
-            "0+" + Integer.MAX_VALUE + ", " + Integer.MAX_VALUE,
-            Integer.MIN_VALUE + "+0, " + Integer.MIN_VALUE,
-            "0+" + Integer.MIN_VALUE + ", " + Integer.MIN_VALUE,
+            MAX_VALUE + "+0, " + MAX_VALUE,
+            "0+" + MAX_VALUE + ", " + MAX_VALUE,
+            MIN_VALUE + "+0, " + MIN_VALUE,
+            "0+" + MIN_VALUE + ", " + MIN_VALUE,
 
             // Three operands
             "1+1+1, 3",
@@ -43,40 +72,25 @@ public class CalculatorTest {
     @Test(expected = IntegerOverflowException.class)
     @Parameters({
             // Both positive
-            Integer.MAX_VALUE + "+1",
-            "1+" + Integer.MAX_VALUE,
+            MAX_VALUE + "+1",
+            "1+" + MAX_VALUE,
             // Both negative
-            Integer.MIN_VALUE + "+-1",
-            "-1+" + Integer.MIN_VALUE,
+            MIN_VALUE + "+-1",
+            "-1+" + MIN_VALUE,
             // Three operands
-            "1+1+" + Integer.MAX_VALUE,
-            "-1+-1+-" + Integer.MIN_VALUE,
+            "1+1+" + MAX_VALUE,
+            "-1+-1+" + MIN_VALUE,
     })
     public void should_not_be_able_to_add_numbers_when_result_rolls_over_the_integer_max_values(String operations) {
-        calculator.compute(operations);
-    }
-
-    @Parameters({"", "    ", "blabla"})
-    @Test(expected = IllegalArgumentException.class)
-    public void should_throw_an_exception_for_operations_not_respecting_the_add_pattern(String operations) {
-        calculator.compute(operations);
+        should_throw_an_exception_when_trying_to_add_something_else_than_numbers(operations);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void should_throw_an_exception_for_a_null_string() {
-        calculator.compute(null);
-    }
-
-    @Test
-    public void should_return_the_number_when_it_is_alone_in_the_operations() {
-        String operations = "1";
-        int result = calculator.compute(operations);
-        assertThat(result).isEqualTo(1);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void should_throw_an_exception_when_the_number_is_bigger_than_the_integer_max_values() {
-        String operations = Integer.MAX_VALUE + "0";
+    @Parameters({
+            "toto+toto",
+            "1+toto",
+    })
+    public void should_throw_an_exception_when_trying_to_add_something_else_than_numbers(String operations) {
         calculator.compute(operations);
     }
 
@@ -92,6 +106,57 @@ public class CalculatorTest {
     public void should_be_able_to_substract(String operations, int expectedResult) {
         int result = calculator.compute(operations);
         assertThat(result).isEqualTo(expectedResult);
+    }
+
+    //--------------------------------------------------------------------------
+    // MULTIPLY
+    //--------------------------------------------------------------------------
+
+
+    @Test
+    @Parameters({
+            "1*1, 1",
+            "2*2, 4",
+            "18*5454, 98172",
+            "1*-2, -2",
+            "-2*1, -2",
+            MAX_VALUE + "*1, " + MAX_VALUE,
+            MAX_VALUE / 2 + "*2, " + (MAX_VALUE - 1),
+            MIN_VALUE + "*1, " + MIN_VALUE,
+            MIN_VALUE / 2 + "*2, " + MIN_VALUE,
+    })
+    public void should_be_able_to_multiply(String operations, int expectedResult) {
+        int result = calculator.compute(operations);
+        assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test(expected = IntegerOverflowException.class)
+    @Parameters({
+            // Both positive
+            (MAX_VALUE / 2 + 1) + "*2",
+            "2*" + (MAX_VALUE / 2 + 1),
+            MAX_VALUE + "*2",
+            "2*" + MAX_VALUE,
+
+            // Positive negative
+            (MIN_VALUE / 2 - 1) + "*2",
+            "2*" + (MIN_VALUE / 2 - 1),
+            MIN_VALUE + "*-2",
+            "-2*" + MIN_VALUE,
+
+            // Both negative
+            (MIN_VALUE / 2 - 1) + "*-2",
+
+//            "1+" + Integer.MAX_VALUE,
+//            // Both negative
+//            Integer.MIN_VALUE + "+-1",
+//            "-1+" + Integer.MIN_VALUE,
+//            // Three operands
+//            "1+1+" + Integer.MAX_VALUE,
+//            "-1+-1+-" + Integer.MIN_VALUE,
+    })
+    public void should_not_be_able_to_multiply_numbers_when_result_rolls_over_the_integer_max_values(String operations) {
+        calculator.compute(operations);
     }
 
 }
