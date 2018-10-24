ab4f5c53d32366a4dd0e1a06383a384d0b3162ef
HadrienMP <public@hadrienmp.fr>
2018-10-24 19:16:35 +0200

    Classify into directories, tired of the mess

--- refactor/gilded_rose/src/test/java/darty/GildedRoseTest.java
+++ refactor/gilded_rose/src/test/java/darty/GildedRoseTest.java
@@ -4,20 +4,23 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
     public static final String GOLDEN_MASTER_PATH = "/home/hadrienmp/Projets/katas/GildedRose/src/test/java/darty/golden-master.txt";
 
     @Test
-    public void golden_master() throws IOException {
-        List<String> goldenMaster = Files.readAllLines(Paths.get(GOLDEN_MASTER_PATH), StandardCharsets.UTF_8);
+    public void golden_master() throws IOException, URISyntaxException {
+        List<String> goldenMaster = Files.readAllLines(
+                Paths.get(
+                        ClassLoader.getSystemResource("darty/golden-master.txt")
+                                .toURI()),
+                StandardCharsets.UTF_8);
         System.out.print(goldenMaster);
 
         List<String> ouput = TextTestFixture.runGoldenMaster(new String[] {});
@@ -27,13 +30,4 @@ public class GildedRoseTest {
         Assertions.assertThat(ouput).isEqualTo(goldenMaster);
 
     }
-
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
-
 }
